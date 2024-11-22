ba15adbe476f2988c3d2a963f9375e253157c5b6
Your Name <you@example.com>
2024-11-22 20:03:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+     // for loop
+      // let x = 1
+      // for (x; x =< 10; x++) {
+       // console.log(x)
+      // }
+
+       //for(let x =10; x >= 1; x--){
+        //console.log(x)
+      // }
+
+       //let x = 20
+       //for(let x = -10; x <= 10; x++){
+       // console.log(x)
+       //}
+
+        //let ticket = 10
+        //for(ticket; ticket >= 0; ticket--){
+        //    console.log(`Welcome.... tickets left: -> ${ticket}`)
+       // }
+
+       // for(let bottle =99; bottle>= 0; bottle--){
+       //     if (bottle == 0){
+        //       myScreen.innerHTML += `<p>No more bottles of beer on the wall.!</p>`
+                
+        //    } else if (bottle == 1){
+        //        myScreen.innerHTML += `<p>${bottle} bottle of beer on the wall, ${bottle} bottle of beer.
+        //            Take one down and pass it around, no more bottles of beer on the wall.</p>`
+        //    }else {     
+        //         myScreen.innerHTML += `<p>${bottle}bottles of beer on the wall, ${bottle} bottles of beer.
+        //            Take one down and pass it around, ${bottle -1 } bottles of beer on the wall.</P>`
+        //    }
+       // }
+
+
+
+       //nested for loop
+       //for(let crate = 1; crate <= 12; crate++){
+        //myScreen.innerHTML += `<p>Crate - ${crate}</p>`
+        //for (let fill =1; fill <= 24; fill++){
+         //   myScreen.innerHTML += `<p style="color: red;">Fill - ${fill}</p>`
+      //  }
+      // }
+
